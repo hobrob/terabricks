0a5938504d94c655a58020a065fdf67c88778d5e
hobrob <hobrob>
2025-08-01 23:40:09 +0000

    Adding markdown cells to deployer notebook

--- temporals/core/register_udfs.sql
+++ temporals/core/register_udfs.sql
@@ -1,4 +1,9 @@
 -- Databricks notebook source
+-- MAGIC %md
+-- MAGIC ### IsPeriod(p)
+-- MAGIC Tests p for being the structure of an array of two or more string elements, and that the first two elements can be cast to either a date, or a time or timestamp of matching precision, and that the upper bound is at least one unit grain of time greater than the lower bound.
+
+-- COMMAND ----------
 
 USE CATALOG IDENTIFIER(:catalog_name);
 USE SCHEMA IDENTIFIER(:schema_name);
