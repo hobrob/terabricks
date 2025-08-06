2ca27b94068c8620103704d44985bb53623deef1
Robert Price <robert.price@rpdw.co.uk>
2025-08-07 00:14:44 +0100

    Linking notepad import with job submission hook

--- temporals/core/register_udfs.sql
+++ temporals/core/register_udfs.sql
@@ -24,3 +24,35 @@ return ping() == "pong"
 $$;
 
 SELECT current_timestamp(), funlib.SmokeTest();
+
+
+
+CREATE OR REPLACE FUNCTION IDENTIFIER(:catalog_name||'.'||:schema_name||'.Contains')(period array<string>, instant string)
+RETURNS BOOLEAN
+LANGUAGE PYTHON
+ENVIRONMENT (
+  dependencies = '["__VOLUME__/__WHEEL__"]',
+  environment_version = 'None'
+)
+AS $$
+
+    from temporals.core.validators import validate_period_array, validate_instant_string
+    from temporals.core.constants import ERRMSG, ERR_UNKNOWN
+
+    validPeriod = validate_period_array(period)
+    if validPeriod:
+        validInstant = validate_instant_string(instant, validPeriod.dttm_type, validPeriod.precision)
+        if validInstant:
+            if validPeriod.start_ts <= validInstant.instant_ts < validPeriod.end_ts:
+                return True
+            else:
+                return False
+    raise RuntimeError(ERRMSG[ERR_UNKNOWN])
+
+$$;
+
+SELECT current_timestamp(), funlib.Contains(array('2025-06-01', '2025-06-30'], '2025-06-15');
+
+
+
+
