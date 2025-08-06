cf6d1cf4faafee24579fa389e21d63f0349ebae3
Robert Price <robert.price@rpdw.co.uk>
2025-08-06 18:51:32 +0100

    Convert wheel build method

--- temporals/core/register_udfs.sql
+++ temporals/core/register_udfs.sql
@@ -15,7 +15,7 @@ CREATE OR REPLACE FUNCTION IDENTIFIER(:catalog_name||'.'||:schema_name||'.SmokeT
 RETURNS BOOLEAN
 LANGUAGE PYTHON
 ENVIRONMENT (
-  dependencies = CONCAT('["', :volume_name, '/', :wheel_name, '"]'),
+  dependencies = '["dbfs:/Volumes/workspace/funlib/terabricks/terabricks_temporals-0.0.1-py3-none-any.whl"]',
   environment_version = 'None'
 )
 AS $$
