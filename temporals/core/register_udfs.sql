4ec8f21ee6841c1d68d3bc9ae17e9d69171043ec
Robert Price <robert.price@rpdw.co.uk>
2025-08-07 07:38:04 +0100

    Linking notepad import with job submission hook

--- temporals/core/register_udfs.sql
+++ temporals/core/register_udfs.sql
@@ -25,7 +25,11 @@ $$;
 
 SELECT current_timestamp(), funlib.SmokeTest();
 
+-- MAGIC %md
+-- MAGIC ### Contains(p, t)
+-- MAGIC Tests t to see if it occurs within the bounds of p, returns true or false.
 
+-- COMMAND ----------
 
 CREATE OR REPLACE FUNCTION IDENTIFIER(:catalog_name||'.'||:schema_name||'.Contains')(period array<string>, instant string)
 RETURNS BOOLEAN
@@ -53,7 +57,11 @@ $$;
 
 SELECT current_timestamp(), funlib.Contains(array('2025-06-01', '2025-06-30'), '2025-06-15');
 
+-- MAGIC %md
+-- MAGIC ### Begin(p)
+-- MAGIC Returns the inclusive lower bound of p.
 
+-- COMMAND ----------
 
 CREATE OR REPLACE FUNCTION IDENTIFIER(:catalog_name||'.'||:schema_name||'.Begin')(period array<string>)
 RETURNS STRING
@@ -64,7 +72,7 @@ ENVIRONMENT (
 )
 AS $$
 
-    from temporals.core.validators import validate_period_array, validate_instant_string
+    from temporals.core.validators import validate_period_array
     from temporals.core.constants import ERRMSG, ERR_UNKNOWN, TEMPRL, DATE, TIME, TIMESTAMP
 
     if period is None:
@@ -88,3 +96,173 @@ AS $$
   $$;
 
 SELECT current_timestamp(), funlib.Begin(array('2025-06-01', '2025-06-30'));
+
+-- MAGIC %md
+-- MAGIC ### End(p)
+-- MAGIC Returns the exclusive upper bound of p.
+
+-- COMMAND ----------
+
+CREATE OR REPLACE FUNCTION IDENTIFIER(:catalog_name||'.'||:schema_name||'.End')(period array<string>)
+RETURNS STRING
+LANGUAGE PYTHON
+ENVIRONMENT (
+  dependencies = '["__VOLUME__/__WHEEL__"]',
+  environment_version = 'None'
+)
+AS $$
+
+    from temporals.core.validators import validate_period_array
+    from temporals.core.constants import ERRMSG, ERR_UNKNOWN, TEMPRL, DATE, TIME, TIMESTAMP
+
+    if period is None:
+        return None
+    validPeriod = validate_period_array(period)
+    if validPeriod:
+        if validPeriod.dttm_type == DATE:
+            return validPeriod.end_ts.strftime(TEMPRL[DATE]["format"])
+        elif validPeriod.dttm_type == TIME:
+            if validPeriod.precision == 0:
+                return validPeriod.end_ts.strftime(TEMPRL[TIME]["format"])
+            else:
+                return validPeriod.end_ts.strftime(TEMPRL[TIME]["format"]+'.%f')
+        elif validPeriod.dttm_type == TIMESTAMP:
+            if validPeriod.precision == 0:
+                return validPeriod.end_ts.strftime(TEMPRL[TIMESTAMP]["format"])
+            else:
+                return validPeriod.end_ts.strftime(TEMPRL[TIMESTAMP]["format"]+'.%f')
+    raise RuntimeError(ERRMSG[ERR_UNKNOWN])
+
+  $$;
+
+SELECT current_timestamp(), funlib.End(array('2025-06-01', '2025-06-30'));
+
+
+-- MAGIC %md
+-- MAGIC ### Last(p)
+-- MAGIC Returns the inclusive upper bound of p.
+
+-- COMMAND ----------
+
+CREATE OR REPLACE FUNCTION IDENTIFIER(:catalog_name||'.'||:schema_name||'.Last')(period array<string>)
+RETURNS STRING
+LANGUAGE PYTHON
+ENVIRONMENT (
+  dependencies = '["__VOLUME__/__WHEEL__"]',
+  environment_version = 'None'
+)
+AS $$
+
+    import math
+    from datetime import date, time, datetime, timedelta
+
+    from temporals.core.validators import validate_period_array
+    from temporals.core.constants import ERRMSG, ERR_UNKNOWN, DATE, TIME, TIMESTAMP
+
+    if period is None:
+        return None
+    validPeriod = validate_period_array(period)
+    if validPeriod:
+        if validPeriod.dttm_type == DATE:
+            return validPeriod.end_ts - timedelta(days=1)
+        elif validPeriod.dttm_type == TIME:
+            calendarised_end_ts = datetime.combine(datetime.today(), validPeriod.end_ts)
+            if validPeriod.precision == 0:
+                return (calendarised_end_ts - timedelta(seconds=1)).time()
+            else:
+                return (calendarised_end_ts - timedelta(microseconds=(math.pow(10, 6 - validPeriod.precision)))).time()
+        elif validPeriod.dttm_type == TIMESTAMP:
+            if validPeriod.precision == 0:
+                return validPeriod.end_ts - timedelta(seconds=1)
+            else:
+                return validPeriod.end_ts - timedelta(microseconds=(math.pow(10, 6 - validPeriod.precision)))
+    raise RuntimeError(ERRMSG[ERR_UNKNOWN])
+
+  $$;
+
+
+-- MAGIC %md
+-- MAGIC ### Overlaps(p1, p2)
+-- MAGIC If p1 and p2 are both valid periods it returns true if p2 overlaps p1, else false. Returns null if either one of p1 or p2 is null.
+
+-- COMMAND ----------
+
+CREATE OR REPLACE FUNCTION IDENTIFIER(:catalog_name||'.'||:schema_name||'.Overlaps')(period1 array<string>, period2 array<string>)
+RETURNS BOOLEAN
+LANGUAGE PYTHON
+ENVIRONMENT (
+  dependencies = '["__VOLUME__/__WHEEL__"]',
+  environment_version = 'None'
+)
+AS $$
+
+    from temporals.core.validators import validate_period_array
+    from temporals.core.constants import ERRMSG, ERR_UNKNOWN
+
+    if period1 is None or period2 is None:
+        return None
+    validPeriod1 = validate_period_array(period1)
+    validPeriod2 = validate_period_array(period2)
+    if validPeriod1 and validPeriod2:
+        return validPeriod1.start_ts <= validPeriod2.end_ts and validPeriod2.start_ts < validPeriod1.end_ts
+    raise RuntimeError(ERRMSG[ERR_UNKNOWN])
+
+  $$;
+
+
+-- MAGIC %md
+-- MAGIC ### OverlapsLeft(p1, p2)
+-- MAGIC If p1 and p2 are both valid periods it returns true if p2 overlaps p1 and the upper bound of p2 is less than or equal to the upper bound of p1, else false. Returns null if either one of p1 or p2 is null.
+
+-- COMMAND ----------
+
+CREATE OR REPLACE FUNCTION IDENTIFIER(:catalog_name||'.'||:schema_name||'.OverlapsLeft')(period1 array<string>, period2 array<string>)
+RETURNS BOOLEAN
+LANGUAGE PYTHON
+ENVIRONMENT (
+  dependencies = '["__VOLUME__/__WHEEL__"]',
+  environment_version = 'None'
+)
+AS $$
+
+    from temporals.core.validators import validate_period_array
+    from temporals.core.constants import ERRMSG, ERR_UNKNOWN
+
+    if period1 is None or period2 is None:
+        return None
+    validPeriod1 = validate_period_array(period1)
+    validPeriod2 = validate_period_array(period2)
+    if validPeriod1 and validPeriod2:
+        return validPeriod1.start_ts <= validPeriod2.end_ts and validPeriod2.start_ts < validPeriod1.end_ts and validPeriod2.end_ts <= validPeriod1.end_ts
+    raise RuntimeError(ERRMSG[ERR_UNKNOWN])
+
+  $$;
+
+-- MAGIC %md
+-- MAGIC ### OverlapsRight(p1, p2)
+-- MAGIC If p1 and p2 are both valid periods it returns true if p2 overlaps p1 and the lower bound of p2 is greater than or equal to the lower bound of p1, else false. Returns null if either one of p1 or p2 is null.
+
+-- COMMAND ----------
+
+CREATE OR REPLACE FUNCTION IDENTIFIER(:catalog_name||'.'||:schema_name||'.OverlapsRight')(period1 array<string>, period2 array<string>)
+RETURNS BOOLEAN
+LANGUAGE PYTHON
+ENVIRONMENT (
+  dependencies = '["__VOLUME__/__WHEEL__"]',
+  environment_version = 'None'
+)
+AS $$
+
+    from temporals.core.validators import validate_period_array
+    from temporals.core.constants import ERRMSG, ERR_UNKNOWN
+
+    if period1 is None or period2 is None:
+        return None
+    validPeriod1 = validate_period_array(period1)
+    validPeriod2 = validate_period_array(period2)
+    if validPeriod1 and validPeriod2:
+        return validPeriod1.start_ts <= validPeriod2.end_ts and validPeriod2.start_ts < validPeriod1.end_ts and validPeriod2.start_ts >= validPeriod1.start_ts
+    raise RuntimeError(ERRMSG[ERR_UNKNOWN])
+
+  $$;
+
