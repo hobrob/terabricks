2490bca36b143f24c854cb7753d051e1803140d1
hobrob <30702647+hobrob@users.noreply.github.com>
2025-09-26 09:03:39 +0100

    Remove unwanted dirs

--- build/lib/temporals/__init__.py
+++
@@ -1,5 +0,0 @@
-from .core.validators import *
-from .core.utils import *
-from .core.constants import *
-
-__all__ = []
--- build/lib/temporals/core/types.py
+++
@@ -1,5 +0,0 @@
-
-from collections import namedtuple
-
-PeriodInfo = namedtuple("PeriodInfo", ["start_ts", "end_ts", "dttm_type", "precision"])
-InstantInfo = namedtuple("InstantInfo", ["instant_ts", "dttm_type", "precision"])
